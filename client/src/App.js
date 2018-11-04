6f57cce61b1090aaabfbb5023c7686238b186cbe
Abdul Said <abdulrahman.asaid@gmail.com>
2018-11-03 21:27:32 -0400

    added routes to react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import NoMatch from './pages/NoMatch';
+import About from './pages/About';
+import Articles from './pages/Articles';
 import FrontPage from './pages/FrontPage';
+import JobPosting from './pages/JobPosting';
+import Meetups from './pages/Meetups';
+import NoMatch from './pages/NoMatch';
+import Resources from './pages/Resources';
+import Login from './pages/Login';
+
 import Nav from './components/Nav';
 
 const App = () => (
@@ -11,6 +18,12 @@ const App = () => (
       <Nav />
       <Switch>
         <Route exact path="/" component={FrontPage} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/articles" component={Articles} />
+        <Route exact path="/jobposting" component={JobPosting} />
+        <Route exact path="/meetups" component={Meetups} />
+        <Route exact path="/resources" component={Resources} />
+        <Route exact path="/login" component={Login} />
         <Route component={NoMatch} />
       </Switch>
     </div>
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,17 +5,23 @@ const Nav = () => (
     <a className="navbar-brand" href="/">
       Home
     </a>
-    <a className="navbar-brand" href="/">
+    <a className="navbar-brand" href="/resources">
       Resources
     </a>
-    <a className="navbar-brand" href="/">
+    <a className="navbar-brand" href="/articles">
       Articles
     </a>
-    <a className="navbar-brand" href="/">
+    <a className="navbar-brand" href="/jobposting">
+      Job Posting
+    </a>
+    <a className="navbar-brand" href="/meetups">
       Meet Ups
     </a>
-    <a className="navbar-brand" href="/">
+    <a className="navbar-brand" href="/login">
       Login
+    </a>
+    <a className="navbar-brand" href="/about">
+      About
     </a>
   </nav>
 );
