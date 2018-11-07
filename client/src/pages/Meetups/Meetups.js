75a944a3ef8c96a969c4b8ffa1c2771eb658b3d0
RubyG <rubyrgill@hotmail.com>
2018-11-07 13:33:58 -0500

    Updated api request using axios

--- client/src/pages/Meetups/Meetups.js
+++ client/src/pages/Meetups/Meetups.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 //Api key
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+// const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 
 
@@ -10,26 +10,35 @@ class Meetups extends Component {
   constructor() {
     super()
     this.state = {
-      title: [],
+      meetups: [],
     }
   }
+  componentWillMount() {
+    this.getMeetUps();
+  }
 
-  //Pulling current events 
-  componentDidMount() {
-    fetch('https://api.meetup.com/find/events?photo-host=public&sig_id=255600544&sig=0b1810a39b3fa52e67d05a8da45babe0f49b7eb0',
-      {
-        headers: {
-          'X-Access-Token': API_KEY,
-          'Content-Type': 'application/json'
-        },
-      }).then(response => {
-        return response.json(); //Json format
-
-      }).then(response => console.log(response))
-      .then(data => {
-        // Specifiy data later
+  getMeetUps() {
+    axios.get('https://api.meetup.com/find/events?photo-host=public&sig_id=255600544&sig=0b1810a39b3fa52e67d05a8da45babe0f49b7eb0')
+      .then(response => {
+        console.log(response.data)
       })
   }
+  //   //Pulling current events 
+  //   componentDidMount() {
+  //     fetch('https://api.meetup.com/find/events?photo-host=public&sig_id=255600544&sig=0b1810a39b3fa52e67d05a8da45babe0f49b7eb0',
+  //       {
+  //         headers: {
+  //           'X-Access-Token': API_KEY,
+  //           'Content-Type': 'application/json'
+  //         },
+  //       }).then(response => {
+  //         return response.json(); //Json format
+
+  //       }).then(response => console.log(response))
+  //       .then(data => {
+  //         // Specifiy data later
+  //       })
+  //   }
 
   render() {
     return (
