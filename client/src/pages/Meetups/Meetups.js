a35abc2c66c290f5aaddef9c87095a02bffce920
RubyG <rubyrgill@hotmail.com>
2018-11-08 20:50:51 -0500

    Updated, displays tech groups

--- client/src/pages/Meetups/Meetups.js
+++ client/src/pages/Meetups/Meetups.js
@@ -11,7 +11,7 @@ class Meetups extends Component {
     super()
     //state
     this.state = {
-      meetups: [],
+      groups: [],
     }
   }
 
@@ -23,10 +23,11 @@ class Meetups extends Component {
 
   getMeetUps() {
     //meetup api
-    axios.get('https://api.meetup.com/find/upcoming_events?topic_category=34&sign=true&photo-host=public&sig_id=255600544&sig=0b1810a39b3fa52e67d05a8da45babe0f49b7eb0&key=' + API_KEY)
+    axios.get('https://api.meetup.com/find/groups?text=tech&sign=true&photo-host=public&sig_id=255600544&sig=0b1810a39b3fa52e67d05a8da45babe0f49b7eb0&key=' + API_KEY)
+      //https://api.meetup.com/find/upcoming_events?topic_category=34&sign=true&photo-host=public&sig_id=255600544&sig=0b1810a39b3fa52e67d05a8da45babe0f49b7eb0&key=
       .then(response => {
         //pull event data
-        this.setState({ meetups: response.data.events }, () => {
+        this.setState({ groups: response.data }, () => {
           console.log(this.state)
         })
 
@@ -34,20 +35,21 @@ class Meetups extends Component {
   }
 
   render() {
-    // console.log(this.state)
     //iterate through the array
-    const meetupItems = this.state.meetups.map((meetup, i) => {
+    const groupItems = this.state.groups.map((group, i) => {
+
       return (
         //display...
-        <div class="container">
-          <div class="eventcard">
-            <div class="card">
-              <h1 class="card-title eventtitle">{meetup.name}</h1>
-              <div class="card-body">
+        <div className="container">
+          <div className="eventcard">
+            <div className="card">
+              <h1 className="card-title eventtitle">{group.name}</h1>
+              <div className="card-body">
                 <ul>
-                  <p class="card-text">Date: {meetup.local_date}</p>
-                  <p class="card-text">Location: {meetup.group.localized_location}</p>
-                  <a href={meetup.link} class="card-link">Link</a>
+                  <p className="card-text">Members: {group.members}</p>
+                  <p className="card-text">Location: {group.localized_location}</p>
+                  <a href={group.link} class="card-link">Link</a>
+                  {/* <img href={group.key_photo.photo_link} alt="nothing"> */}
                 </ul>
                 <hr></hr>
               </div>
@@ -59,7 +61,7 @@ class Meetups extends Component {
     return (
       // render to div
       <h1>
-        {meetupItems}
+        {groupItems}
       </h1>
     )
   }
