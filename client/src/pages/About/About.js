d11921238d2cb7f8a872be889c1c36be10944f5f
RubyG <rubyrgill@hotmail.com>
2018-11-22 20:45:02 -0500

    Updated about page and fav page

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import './aboutstyle.css';
 
 class About extends Component {
   render() {
-    return <h1>This is the About page</h1>;
+    return <div id="container">
+      <div class="aboutcontainer">
+
+
+
+        <ul id="about-ul">
+          <li class="about-li a">ABOUT DEVBUDDY</li>
+          {/* <li class="about-li b">B</li>
+        <li class="about-li o">O</li>
+        <li class="about-li u">U</li>
+        <li class="about-li t">T</li> */}
+        </ul>
+
+
+        <hr></hr>
+
+      </div>
+      <div class="about-body">
+        Blahh
+        </div>
+    </div >
   }
 }
 
--- client/src/pages/Favourite/Favourite.js
+++ client/src/pages/Favourite/Favourite.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import './favstyle.css';
 
 const Favourite = () => {
-  return <h1>Favourite Page</h1>;
+  return <div className="container">
+    <div className="saved-deck">
+      <h1>EVENTS</h1>
+      <div className="scrollmenu">
+        <a className="saveditem" href="#home">Junior Tech Summit</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+      </div>
+    </div>
+    <div className="saved-deck">
+      <h1>EVENTS</h1>
+      <div className="scrollmenu">
+        <a className="saveditem" href="#home">Junior Tech Summit</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+        <a className="saveditem" href="#home">Home</a>
+      </div>
+    </div>
+  </div>
+
 };
 
 export default Favourite;
