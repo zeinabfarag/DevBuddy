34ed9713e62b29cb4edf2b5368ce58ddefcaae06
RubyG <rubyrgill@hotmail.com>
2018-11-24 01:17:43 -0500

    Updated about layout

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,30 +1,125 @@
 import React, { Component } from 'react';
 import './aboutstyle.css';
 
+
+
+
 class About extends Component {
   render() {
-    return <div id="container">
-      <div class="aboutcontainer">
-
-
-
-        <ul id="about-ul">
-          <li class="about-li a">ABOUT DEVBUDDY</li>
-          {/* <li class="about-li b">B</li>
-        <li class="about-li o">O</li>
-        <li class="about-li u">U</li>
-        <li class="about-li t">T</li> */}
-        </ul>
-
-
-        <hr></hr>
-
-      </div>
-      <div class="about-body">
-        Blahh
+    return <div className="about">
+      <div className="container">
+        <div className="saved-deck">
+          <h1 className="saved-section about-title">ABOUT DEVBUDDY</h1>
         </div>
-    </div >
-  }
-}
+      </div>
+      <div className="container saved-container">
+        <div className="saved-deck">
+          {/* <h1 className="saved-section">PRESS PLAY</h1> */}
+          <div class="videoWrapper">
+            {/* <!-- Copy & Pasted from YouTube --> */}
+            <iframe width="560" height="349" src="http://www.youtube.com/embed/n_dZNLr2cME?rel=0&hd=1" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+      <div className="about-container saved-container">
+        <div className="saved-deck">
+          <div className="container">
+            <h1 className="saved-section about-title">MEET THE TEAM</h1>
+            <div class="teamdev">
+              <div id="ateam" class="teammembers">
+
+                <div className="memberimage col-2">
+                  <img className="team-img" src="https://files.slack.com/files-pri/TA2JAF9B8-FE0THTC1Z/debuddy.png" width="250px"></img>
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Bob</h1>
+                  <p className="abouttext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum eros eget hendrerit bibendum.  </p>
+                  <div className="contact-icons">
+                    <a href="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png">
+                      <img className="github-img" src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png" width="50px"></img>
+                    </a>
+
+                  </div>
+
+                </div>
+              </div>
+              <div id="bteam" class="teammembers">
+
+                <div className="memberimage col-2">
+                  <img className="team-img" src="https://files.slack.com/files-pri/TA2JAF9B8-FE0THTC1Z/debuddy.png" width="250px"></img>
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Bob</h1>
+                  <p className="abouttext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum eros eget hendrerit bibendum.  </p>
+                  <div className="contact-icons">
+                    <a href="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png">
+                      <img className="github-img" src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png" width="50px"></img>
+                    </a>
+
+                  </div>
+                </div>
+              </div>
+              <div id="cteam" class="teammembers">
+
+                <div className="memberimage col-2">
+                  <img className="team-img" src="https://files.slack.com/files-pri/TA2JAF9B8-FE0THTC1Z/debuddy.png" width="250px"></img>
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Bob</h1>
+                  <p className="abouttext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum eros eget hendrerit bibendum.  </p>
+                  <div className="contact-icons">
+                    <a href="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png">
+                      <img className="github-img" src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png" width="50px"></img>
+                    </a>
+
+                  </div>
+                </div>
+              </div>
+              <div id="adteam" class="teammembers">
+
+                <div className="memberimage col-2">
+                  <img className="team-img" src="https://files.slack.com/files-pri/TA2JAF9B8-FE0THTC1Z/debuddy.png" width="250px"></img>
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Bob</h1>
+                  <p className="abouttext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum eros eget hendrerit bibendum.  </p>
+                  <div className="contact-icons">
+                    <a href="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png">
+                      <img className="github-img" src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png" width="50px"></img>
+                    </a>
+
+                  </div>
+                </div>
+              </div>
+              <div id="eteam" class="teammembers">
+
+                <div className="memberimage col-2">
+                  <img className="team-img" src="https://files.slack.com/files-pri/TA2JAF9B8-FE0THTC1Z/debuddy.png" width="250px"></img>
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Bob</h1>
+                  <p className="abouttext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum eros eget hendrerit bibendum.  </p>
+                  <div className="contact-icons">
+                    <a href="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png">
+                      <img className="github-img" src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png" width="50px"></img>
+                    </a>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>
+
+  };
+};
 
 export default About;
--- client/src/pages/Favourite/Favourite.js
+++ client/src/pages/Favourite/Favourite.js
@@ -2,41 +2,68 @@ import React from 'react';
 import './favstyle.css';
 
 const Favourite = () => {
+
+
   return <div className="container">
-    <div className="saved-deck">
-      <h1>EVENTS</h1>
-      <div className="scrollmenu">
-        <a className="saveditem" href="#home">Junior Tech Summit</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
+    <h1 id="favtitle">FAVOURITES</h1>
+    <div className=" saved-container">
+      <div className="saved-deck">
+        <h1 className="saved-section">JOB POSTINGS</h1>
+        <div className="scrollmenu">
+          <a className="saveditem" href="#home">Junior Tech Summit</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+        </div>
       </div>
     </div>
-    <div className="saved-deck">
-      <h1>EVENTS</h1>
-      <div className="scrollmenu">
-        <a className="saveditem" href="#home">Junior Tech Summit</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
-        <a className="saveditem" href="#home">Home</a>
+    <div className=" saved-container">
+      <div className="saved-deck">
+        <h1 className="saved-section">ARTICLES</h1>
+        <div className="scrollmenu">
+          <a className="saveditem" href="#home">How To be A Dev</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+        </div>
       </div>
     </div>
+    <div className=" saved-container">
+      <div className="saved-deck">
+        <h1 className="saved-section">MEETUPS</h1>
+        <div className="scrollmenu">
+          <a className="saveditem" href="#home">How To be A Dev</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+          <a className="saveditem" href="#home">Home</a>
+        </div>
+      </div>
+    </div>
+
   </div>
 
 };
