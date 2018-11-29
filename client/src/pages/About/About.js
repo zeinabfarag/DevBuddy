27ac8021704ad0ce130042f8c46daea72b4825bd
Zee <zzeinabfaragg@gmail.com>
2018-11-29 16:28:52 -0500

    aboutpage

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,150 +1,102 @@
-import React, { Component } from 'react';
-import './aboutstyle.css';
+import React, { Component } from "react";
+import "./aboutstyle.css";
 
 class About extends Component {
   render() {
     return (
-      <div className="about">
-        <div className="container">
-          <div className="saved-deck">
-            <h1 className="saved-section about-title">ABOUT DEVBUDDY</h1>
-          </div>
+      <div className="container">
+        <div id="main">
+          <h1 id="maintitle"> About Devbuddy</h1>
+          <p id="description">
+            <span id="firstword"> Learning</span> how to code can be a daunting
+            task when you first start. However, we have all come from different
+            educational backgrounds and managed to become full MERN stack web
+            developers as well as created this site you see now. Learn more
+            about how we got started!
+          </p>
         </div>
-        <div className="container saved-container">
-          <div className="saved-deck">
-            {/* <h1 className="saved-section">PRESS PLAY</h1> */}
-            <div className="videoWrapper">
-              <iframe
-                title="titlewrapper"
-                width="560"
-                height="349"
-                src="/asset/images/Dev_1.mp4"
-                frameBorder="0"
-                allowFullScreen
-              />
-            </div>
-          </div>
-          <div className="about-container saved-container">
-            <div className="saved-deck">
-              <div className="container">
-                <h1 className="saved-section about-title">MEET THE TEAM</h1>
-                <div className="teamdev">
-                  <div id="ateam" className="teammembers">
-                    <div className="memberimage col-2">
-                      <img
-                        className="team-img"
-                        src="/asset/images/ab.png"
-                        width="300px"
-                        alt=""
-                      />
-                    </div>
-                    <div className="member-info col-8">
-                      <h1 className="member-section">Abdulrahman Said</h1>
-                      <p className="abouttext">
-                        Graduated from Ryerson with a Bachelor of Commerce.
-                        Getting ready to jump start my career as a new Dev!
-                        Started with taking a Full Stack Development course at
-                        UTM focusing on the MERN (Mongo, React, Express,
-                        Node.js) stack. 6 months later, I've created multple
-                        projects using different technologies that I am really
-                        proud of.{' '}
-                      </p>
-                      <div className="contact-icons">
-                        <a href="https://github.com/AbdulSaid">
-                          <img
-                            className="github-img"
-                            src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png"
-                            width="50px"
-                            alt=""
-                          />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div id="zteam" className="teammembers">
-                    <div className="memberimage col-2">
-                      <img
-                      className="team-img"
-                      src="/asset/images/zteam.png"
-                      width="300px"
-                      alt=""
-                      />
-                    </div>
-                    <div className="member-info col-8">
-                      <h1 className="member-section">Zeinab Farag</h1>
-                      <p className="abouttext">
-                      Lorem Ipsum.{' '}
-                      </p>
-                      <div className="contact-icons">
-                        <a href="https://github.com/zeinabfarag">
-                          <img
-                          className="github-img"
-                          src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png"
-                          width="50px"
-                          alt=""
-                          />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div id="cteam" className="teammembers">
-                    <div className="memberimage col-2">
-                      <img
-                      className="team-img"
-                      src="/asset/images/al.png"
-                      width="300px"
-                      alt=""
-                      />
-                    </div>
-                    <div className="member-info col-8">
-                      <h1 className="member-section">Alejandra Juarez</h1>
-                      <p className="abouttext">
-                      Graduate from Computer Science. 11 years of IT experience
-                      in mainframe with Cobol. Decided to update my skills with
-                      a Business Analyst and Coding Bootcamp from UTM.
-                      Eager to demonstrate my acquired knowledge on new
-                      challenges.{' '}
-                      </p>
-                      <div className="contact-icons">
-                        <a href="https://github.com/alejuarez">
-                        <img
-                          className="github-img"
-                          src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png"
-                          width="50px"
-                          alt=""
-                        />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div id="adteam" className="teammembers">
-                    <div className="memberimage col-2">
-                      <img
-                      className="team-img"
-                      src="/asset/images/r.png"
-                      width="300px"
-                      alt=""
-                      />
-                    </div>
-                    <div className="member-info col-8">
-                      <h1 className="member-section">Ruby Gill</h1>
-                      <p className="abouttext">
-                      Background in digital marketing and research. I look
-                      forward to applying best practices learned in the UTM Full
-                      Stack Bootcamp in future projects.{' '}
-                      </p>
-                      <div className="contact-icons">
-                        <a href="https://github.com/Rubyrgill">
-                        <img
-                          className="github-img"
-                          src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png"
-                          width="50px"
-                          alt=""
-                        />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+        <div className="maincontainer">
+          <div className="container">
+            <h1 id="maintitle"> Meet the Team!</h1>
+
+            <div className="teamdev">
+              <div id="ateam" className="teammembers">
+                <div className="memberimage col-2">
+                  <img
+                    className="team-img"
+                    src="/asset/images/ab.png"
+                    width="300px"
+                    alt=""
+                  />
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Abdulrahman Said</h1>
+                  <p className="abouttext">
+                    Graduated from Ryerson with a Bachelor of Commerce. Getting
+                    ready to jump start my career as a new Dev! Started with
+                    taking a Full Stack Development course at UTM focusing on
+                    the MERN (Mongo, React, Express, Node.js) stack. 6 months
+                    later, I've created multiple projects using different
+                    technologies that I am really proud of.
+                  </p>
+                </div>
+              </div>
+              <div id="zteam" className="teammembers">
+                <div className="memberimage col-2">
+                  <img
+                    className="team-img"
+                    src="/asset/images/zteam.png"
+                    width="300px"
+                    alt=""
+                  />
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Zeinab Farag</h1>
+                  <p className="abouttext">
+                    Started off in life sciences and not so quickly realized it
+                    wasn't for me. Began to study coding on my own and after
+                    watching countless youtube videos and doing a lot of
+                    tutorials, I realized this is what I want to be doing. Have
+                    now completed a 6 month bootcamp course and ready to start
+                    my career in web development.
+                  </p>
+                </div>
+              </div>
+              <div id="cteam" className="teammembers">
+                <div className="memberimage col-2">
+                  <img
+                    className="team-img"
+                    src="/asset/images/al.png"
+                    width="300px"
+                    alt=""
+                  />
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Alejandra Juarez</h1>
+                  <p className="abouttext">
+                    Graduate from Computer Science. 11 years of IT experience in
+                    mainframe with Cobol. Decided to update my skills with a
+                    Business Analyst and Coding Bootcamp from UTM. Eager to
+                    demonstrate my acquired knowledge on new challenges.{" "}
+                  </p>
+                </div>
+              </div>
+              <div id="adteam" className="teammembers">
+                <div className="memberimage col-2">
+                  <img
+                    className="team-img"
+                    src="/asset/images/r.png"
+                    width="300px"
+                    alt=""
+                  />
+                </div>
+                <div className="member-info col-8">
+                  <h1 className="member-section">Ruby Gill</h1>
+                  <p className="abouttext">
+                    Background in digital marketing and research. I look forward
+                    to applying best practices learned in the UTM Full Stack
+                    Bootcamp in future projects.{" "}
+                  </p>
                 </div>
               </div>
             </div>
