88e79ff0d36399d62d0a11d329e5122b0a6fd1dc
Abdul Said <abdulrahman.asaid@gmail.com>
2018-11-30 09:54:31 -0500

    added validation to login and signup

--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import axios from "axios";
-import "./SignupForm.css";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import './SignupForm.css';
 
 class SignupForm extends Component {
   constructor() {
     super();
     this.state = {
-      username: "",
-      password: "",
+      username: '',
+      password: '',
       redirectTo: null
     };
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,24 +21,33 @@ class SignupForm extends Component {
     });
   }
   handleSubmit(event) {
-    console.log("sign-up handleSubmit, username: ");
+    console.log('sign-up handleSubmit, username: ');
     console.log(this.state.username);
     event.preventDefault();
 
     //request to server to add a new username/password
     axios
-      .post("/user/", {
+      .post('/user/', {
         username: this.state.username,
         password: this.state.password
       })
       .then(response => {
         console.log(response);
         if (!this.state.username || !this.state.password) {
-          alert("Please fill out Username and Password");
+          alert('Please fill out Username and Password');
         } else if (this.state.password.length < 6) {
-          alert(`Choose a password with more than 6 letters`);
-        } else if (response.status === 200) {
-          console.log("successful signup");
+          // Check length of password
+          alert(`Choose a password with more than 5 letters`);
+        } else if (response.data.errors) {
+          // Checks if its email
+          console.log(response.data.message);
+          alert(response.data.message);
+        } else if (response.data.error) {
+          // User exists error
+          console.log(response.data.error);
+          alert(response.data.error);
+        } else if (!response.data.errors) {
+          console.log('successful signup');
           alert("You've Successfully signed up.");
           // update App.js state
           this.props.loginUser({
@@ -47,14 +56,14 @@ class SignupForm extends Component {
           });
           this.setState({
             //redirect to login page
-            redirectTo: "/"
+            redirectTo: '/'
           });
         } else {
-          console.log("username already taken");
+          console.log('username already taken');
         }
       })
       .catch(error => {
-        console.log("signup error: ");
+        console.log('signup error: ');
         console.log(error);
       });
   }
